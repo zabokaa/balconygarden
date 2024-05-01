6cc0363c72c980737bc9f73dfe994e610d1192c1
zabokaa <sab_fem@gmx.de>
2024-05-01 20:00:19 +0200

    create templates/includes folder

--- boutique/urls.py
+++ boutique/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('home/', include('home.urls')),
+    # important: adding static to urlpatterns
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
