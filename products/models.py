ee7cbf20ecf330ac0e586f48ef48ea2bf55eb394
zabokaa <sab_fem@gmx.de>
2024-05-01 21:06:10 +0200

    create app products + models + add fixtures

--- products/models.py
+++ products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Models
+
+class Category(models.Model):
+    """A category model that includes a name and a friendly name."""
+    name = models.CharField(max_length=254)
+    # add a friendly name field to the category model, that is optional
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+# string methods
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Product(models.Model):
+    """A product model that includes a name, description, price, category, and image."""
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name    
