92aadc03b44cfa251bda408ff83aead64667d317
zabokaa <sab_fem@gmx.de>
2024-05-02 21:42:25 +0200

    add path for product_details

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    # will contain the product id !
+    path('<product_id>', views.product_details, name='product_details'),
 ]
