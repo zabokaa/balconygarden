7010e9214241195eabeef1deefdef2db73bda1bd
zabokaa <sab_fem@gmx.de>
2024-05-03 19:00:22 +0200

    add sorting functionality by price/rating/category

--- products/views.py
+++ products/views.py
@@ -8,13 +8,32 @@ def all_products(request):
     """" A view to return the index page of products, 
     including sorting and search queries"""
     products = Product.objects.all()
+        # If the key does not exist, None is returned
     query = None
     categories = None
+    sort = None
+    direction = None
+
     if request.GET:
-        if 'category' in request.GET:
-            # request.GET is a dictionary-like object that allows you to access the data sent in the HTTP request by key name
-            # get() is a method that returns the value of the specified key
-            # If the key does not exist, None is returned
+        if 'sort' in request.GET:
+            # sortkey is the key that we are sorting by
+            sortkey = request.GET['sort']
+            # if the key is name, we are sorting by name
+            # creating sortkey so we do not changing the original name variable
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                # using lower built-in fct to make the sorting case-insensitive
+                products = products.annotate(lower_name=Lower('name'))
+            # if the key is direction, we are sorting by direction
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                # if the direction is descending, we are adding a minus sign in front of the sortkey
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            # sorting the products by the sortkey with order-by method
+            products = products.order_by(sortkey)
+        if 'category' in request.GET:    
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             # converting list of strings in an acutal list of objects, so we can enter them in the filter
@@ -30,11 +49,14 @@ def all_products(request):
             # | is the or operator
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+# return current sorting mehod to the template
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
