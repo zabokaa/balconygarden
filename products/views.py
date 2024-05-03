f062a72161833bee5b1c5235a1c5c66540fcec3d
zabokaa <sab_fem@gmx.de>
2024-05-03 19:34:55 +0200

    correct render in product_detail views

--- products/views.py
+++ products/views.py
@@ -66,4 +66,4 @@ def product_detail(request, product_id):
     context = {
         'product': product,
     }
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
