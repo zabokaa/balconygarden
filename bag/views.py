271b08f232f6ecfaec6774dc87110e265fefff4d
zabokaa <sab_fem@gmx.de>
2024-05-09 23:39:05 +0200

    add size if statements to bag view

--- bag/views.py
+++ bag/views.py
@@ -10,13 +10,28 @@ def add_to_bag(request, item_id):
 # adding quantity and trasnform it to int as it is a string
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     # storing the bag in the session and get the bag if it exists
     bag = request.session.get('bag', {})
-# of theres exists already an item in the session 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+
+    if size:
+        # if the item is already in the bag
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+                # if the item is not in the bag
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+# of theres exists already an item in the session
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
