f50b52f40103da409eeb072f8c39018f2769785c
zabokaa <sab_fem@gmx.de>
2024-05-16 16:39:10 +0200

    add message+storage to settings

--- boutique/settings.py
+++ boutique/settings.py
@@ -67,6 +67,10 @@ TEMPLATES = [
     },
 ]
 
+# often not required:
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
