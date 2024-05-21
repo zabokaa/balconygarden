f9ea647d3c5739b57ffb7f15ff71ac84dbe0161d
zabokaa <sab_fem@gmx.de>
2024-05-21 02:50:31 +0200

    create own static checkout.css for checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -5,15 +5,19 @@ from .forms import OrderForm
 
 
 def checkout(request):
+    """
+    A view to return the checkout page
+    """
+    # get bag from session + check if it's empty
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-
+# create instance for order form
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
     }
-
+# rendering it all out
     return render(request, template, context)
