c00426f55824ffb1a6074e038f2c1c6691f1e84e
zabokaa <sab_fem@gmx.de>
2024-05-21 02:42:35 +0200

    add crispy from to settings (incl. builtins)

--- boutique/settings.py
+++ boutique/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -64,6 +67,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
