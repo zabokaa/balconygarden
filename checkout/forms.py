88bcdf08d5e79446afe70322fc667eefc161380d
zabokaa <sab_fem@gmx.de>
2024-05-22 22:00:48 +0200

    add if field != country for avoiding error

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,6 +30,12 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
