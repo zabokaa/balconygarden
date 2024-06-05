81f637dc87fae499aae69339e6d00c2da3337da5
zabokaa <sab_fem@gmx.de>
2024-06-05 22:12:31 +0200

    add forms.py to products app

--- boutique/settings.py
+++ boutique/settings.py
@@ -181,4 +181,5 @@ load_dotenv()
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_CURRENCY = 'usd'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'boutique@example.com'
