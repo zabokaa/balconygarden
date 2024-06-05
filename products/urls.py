5f33991921141921b3bafb96407bf13ffa0c286d
zabokaa <sab_fem@gmx.de>
2024-06-05 22:32:54 +0200

    add explanatory comment to last commit

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    # We'll also need to update the product detail URL slightly
+# to specify that the product ID should be an integer.
+# Since otherwise navigating to products /add will interpret the string add as a product id.
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
 ]
