61d005b02b04c74f2a17430a615fb149c3a529ae
zabokaa <sab_fem@gmx.de>
2024-06-05 21:44:19 +0200

    add send_mail function to send_conf_emai attribute

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,6 +3,7 @@ import json
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 # import logging
 import stripe
 from products.models import Product
@@ -19,7 +20,21 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        
+        cust_email = order.email
+        # render_to_string is a Django function that turns a template into HTML
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        # uing the send_mail function to send the email
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """
