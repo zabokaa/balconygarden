f9c12aeb82f48616c2636bd31598301e9662dfa2
zabokaa <sab_fem@gmx.de>
2024-06-05 22:43:05 +0200

    add comments to products app

--- products/urls.py
+++ products/urls.py
@@ -2,11 +2,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.all_products, name='products'),
-    # We'll also need to update the product detail URL slightly
+# We'll also need to update the product detail URL slightly
 # to specify that the product ID should be an integer.
 # Since otherwise navigating to products /add will interpret the string add as a product id.
-    path('<int:product_id>', views.product_detail, name='product_detail'),
+# This happens because django will always use the first URL it finds a matching pattern for.
+# And in this case unless we specify that product id is an integer
+# django doesn't know the difference between a product number and a string like, add.
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
 ]
