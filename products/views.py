b9a54df686995e71be704fa9edcbb93430950b5e
zabokaa <sab_fem@gmx.de>
2024-06-10 10:27:10 +0200

    add in_stock check to product_details

--- products/views.py
+++ products/views.py
@@ -74,8 +74,10 @@ def all_products(request):
 def product_detail(request, product_id):
     """" A view to show individual product details"""
     product = get_object_or_404(Product, pk=product_id)
+    inventory = Inventory.objects.get(product=product)
     context = {
         'product': product,
+        'inventory': inventory,
     }
     return render(request, 'products/product_detail.html', context)
 
