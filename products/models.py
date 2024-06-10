fa06026aaaac352b9bf499c9c8f4d6ab4e785cde
zabokaa <sab_fem@gmx.de>
2024-06-10 06:19:36 +0200

    add docstring for Inventory

--- products/models.py
+++ products/models.py
@@ -28,11 +28,18 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
- 
     def __str__(self):
         return self.name
 
 class Inventory(models.Model):
+    """
+    The Inventory model represents the stock inventory for a product.
+
+    Fields:
+    product (ForeignKey): A reference to the associated Product instance.
+    in_stock (IntegerField): The quantity of the product currently in stock. Defaults to 0.
+    last_updated (DateTimeField): The date and time when the inventory was last updated.
+    """
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     in_stock = models.IntegerField(default=0)
     last_updated = models.DateTimeField()
