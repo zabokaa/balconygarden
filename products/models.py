28a3e49b21582b4aa44f1dc2e6f7a674290a0e8a
zabokaa <sab_fem@gmx.de>
2024-06-10 06:18:04 +0200

    add inventory model

--- products/models.py
+++ products/models.py
@@ -8,8 +8,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
-    # add a friendly name field to the category model, that is optional
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
 
 # string methods
     def __str__(self):
@@ -24,12 +24,15 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    # to understand the concept
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
+ 
     def __str__(self):
-        return self.name    
+        return self.name
+
+class Inventory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    in_stock = models.IntegerField(default=0)
+    last_updated = models.DateTimeField()
